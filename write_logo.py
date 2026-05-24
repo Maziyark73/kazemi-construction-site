b69846220ae20bd67e14bc7e32d7adfe12ee0a56
Maziyar Khademi-astaneh <maziyar@Maziyars-MacBook-Pro.local>
2026-05-24 04:30:05 -0700

    Update site to match mockup

--- write_logo.py
+++
@@ -1,7 +0,0 @@
-import base64, os
-
-data = """DATA_GOES_HERE"""
-
-with open('/Users/maziyar/kazemi-construction-site/public/logo.png', 'wb') as f:
-    f.write(base64.b64decode(data))
-print('Logo written!')
